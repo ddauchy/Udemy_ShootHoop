60b1522163538d777a67a4fbe16de1a20e0cb9af
Daniel Dauchy <danieldauchy@Daniels-MacBook-Air.local>
2016-09-27 22:14:28 -0400

    first commit

---
+++ Assets/scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousex = Input.GetAxis ("Mouse X");
+		float mousey = Input.GetAxis ("Mouse Y");
+		float rotationSpeed = 5.0f;
+			
+		transform.localRotation = Quaternion.Euler (0, mousex*rotationSpeed, 0) * transform.localRotation;
+		Camera camera = GetComponentInChildren<Camera> ();	
+		camera.transform.localRotation = camera.transform.localRotation * Quaternion.Euler (-mousey*rotationSpeed, 0, 0);
+
+	}
+}
