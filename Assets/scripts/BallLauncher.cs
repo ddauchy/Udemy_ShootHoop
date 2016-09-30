52c7cd5ada80e8d87ce7c9bc90a000b5abcc258c
PurpleFinch <dandauchy@gmail.com>
2016-09-29 20:06:12 -0400

    switching to mac

---
+++ Assets/scripts/BallLauncher.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			instance.transform.position = transform.position;
+
+			Camera camera = GetComponentInChildren<Camera> ();
+			rb.velocity = camera.transform.localRotation * (Vector3.forward * speed);
+		}
+	}
+}
